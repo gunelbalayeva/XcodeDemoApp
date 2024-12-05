68367f0ad02e93415e40f3615efbf84f8665e0cc
gunelbalayeva <gunelbalayeva97@gmail.com>
2024-12-05 16:10:53 +0400

    Initial Commit

---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by User on 05.12.24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
